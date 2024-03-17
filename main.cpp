8b5650e17ad41650ecadfc532af46d82d17c7bcf
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-17 17:22:26 +0200

    Ex1 was done

--- main.cpp
+++ main.cpp
@@ -2,4 +2,21 @@
 #include <ctime>
 using namespace std;
 
+string delete_(char* str, int k)
+{
+	while (str[k] != '\0')
+	{
+		str[k] = str[k + 1];
+		k++;
+	}
+	return str;
+}
 
+
+	
+int main()
+{
+	char str[] = "Hallo world";
+	cout << delete_(str, 4);
+	return 0;
+}
