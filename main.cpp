633ef547dee988a274a6d81743f7860885d0c64d
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-17 17:44:46 +0200

    Ex2 was done

--- main.cpp
+++ main.cpp
@@ -3,3 +3,32 @@
 using namespace std;
 
 
+char* delete_2(char* str,char* sign)
+{
+	int i = 0, j = 0;
+	while (str[i] != '\0')
+	{
+		if ((int)str[i] == (int)sign[0])
+		{
+			j = i;
+			while (str[j] != '\0')
+			{
+				str[j] = str[j + 1];
+				j++;
+			}
+		}
+		i++;
+		j = 0;
+	}
+	return str;
+}
+
+
+
+int main()
+{
+	char str[] = "Hallo world";
+	char sign[] = "o";
+	cout << delete_2(str, sign);
+	return 0;
+}
